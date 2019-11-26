7614bb97c9d3289448c38b4fd0c2afc8c380d1a7
AkshayKajale <31135626+AkshayKajale@users.noreply.github.com>
2019-11-25 17:00:51 -0800

    Add files via upload

---
+++ printMaxTime.java
@@ -0,0 +1,64 @@
+
+
+
+public class printMaxTime {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		String str = "2?:?8";
+		
+		if(str.contains("??:??"))
+		{
+			System.out.println("23;59");
+		}
+		
+		char[] arr = str.toCharArray();
+		
+		for(int i = 0 ; i<arr.length ; i++)
+		{
+			if(arr[i] == '?')
+			{
+				if(i == 0)
+				{
+					if(Character.getNumericValue(arr[i+1])<=3)
+					{
+						arr[i] = '2';
+					}
+					else
+					{
+						arr[i] = '1';
+					}
+				}
+				if(i == 1)
+				{
+					if(Character.getNumericValue(arr[i-1]) == 1)
+					{
+						arr[i] = '9';
+					}
+					else
+					{
+						arr[i] = '3';
+					}
+				}
+				
+				if(i == 3)
+				{
+					arr[i] ='5';
+				}
+				
+				if(i == 4)
+				{
+					arr[i] = '9';
+				}
+			}
+		}
+		
+		String str1 = new String(arr);
+		
+		System.out.println(str1);
+		
+}
+}
